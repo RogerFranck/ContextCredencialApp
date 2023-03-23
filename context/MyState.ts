4e7178dceb99bf5347731479265b8a8aa8587212
miguel tostado <mtalamo2000@gmail.com>
2023-03-23 11:43:50 -0600

    Correccion xd

--- context/MyState.ts
+++ context/MyState.ts
@@ -1,29 +1,38 @@
+import { useReducer } from "react";
 import { employee } from "../Interface/employee";
+import MyReducer from "./MyReducer";
+import MyContext from "./MyContext";
 
-type Action = {
-  type: "CHANGE_INFO";
-  payload: {
-    key: string;
-    value: any;
+export default function MyState({ children }: any) {
+  const initialState: employee = {
+    nombre: "Eren Yeager",
+    fechaNacimiento: "3/30/850",
+    puesto: "Titan de ataque",
+    email: "ErenYeager@eldia.com",
+    telefono: "9999999999",
+    foto: "https://super-ficcion.com/wp-content/uploads/2022/02/Eren-Jaeger-780x470.webp",
   };
-};
 
-const initialState: employee = {
-  nombre: "Eren Yeager",
-  fechaNacimiento: "3/30/850",
-  puesto: "Titan de ataque",
-  email: "ErenYeager@eldia.com",
-  telefono: "9999999999",
-  foto: "https://super-ficcion.com/wp-content/uploads/2022/02/Eren-Jaeger-780x470.webp",
-};
+  const [state, dispatch] = useReducer(MyReducer, initialState);
 
-function reducer(state: employee, action: Action): employee {
-  switch (action.type) {
-    case "CHANGE_INFO":
-      return { ...state, [action.payload.key]: action.payload.value };
-    default:
-      throw new Error("Invalid action type");
-  }
+  const ChangeInfo = (key: string, value: any) => {
+    dispatch({
+      type: "CHANGE_INFO",
+      payload: {
+        key,
+        value,
+      },
+    });
+  };
+
+  return (
+    <MyContext.Provider
+      value={{
+        ...state,
+        ChangeInfo,
+      }}
+    >
+      {children}
+    </MyContext.Provider>
+  );
 }
-
-export { reducer, initialState };
